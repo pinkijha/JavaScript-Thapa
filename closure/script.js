c2639094be7b49b5b65a80da1aa99b4c8cb02053
Pinki Jha <pinkijha1310@gmail.com>
2025-03-02 23:59:16 +0530

    add closure

--- closure/script.js
+++ closure/script.js
@@ -1,3 +1,13 @@
-function hello() {
-    alert('hello')
-}
+function outer() {
+    let count = 0;
+    
+    return function inner() {
+      count++;
+      console.log(count);
+    };
+  }
+  
+  const increment = outer();
+  increment(); // ✅ 1
+  increment(); // ✅ 2 (Remembers count variable)
+  
